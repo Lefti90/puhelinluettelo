a733d5856f94059b55a34907aebeaffb04c71760
Lefti90 <55137561+Lefti90@users.noreply.github.com>
2022-07-14 19:17:40 +0300

    2.6 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Person from './components/Person'
 
-function App() {
+const App = (props) => {
+  const [persons, setPersons] = useState(props.personArr) 
+  const [newName, setNewName] = useState('')
+  //const [showAll, setShowAll] = useState(true)
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObj = {
+      id: persons.length + 1,
+      name: newName,
+    }
+
+    setPersons(persons.concat(personObj))
+    console.log(personObj)
+    setNewName('')
+  }
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  //const personsToShow = showAll ? persons : persons
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {/* <div>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'all' : 'all' }
+          </button>
+        </div>   */}
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+         name : <input value={newName} onChange={handlePersonChange} />
+         <button type="submit">add</button>
+        <div>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <Person key={person.id} person={person}/>)}        
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
